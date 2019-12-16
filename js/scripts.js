4a66698cedb049a9bf58e6c42fc0669486effdd2
Tomasz <tomekwebdev@gmail.com>
2019-12-16 23:42:37 +0100

    done !

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,19 @@ $(document).ready(function(){
   });
 
 });
+
+var logo = document.querySelector("#con-logo");
+
+(function() {
+
+  window.addEventListener("scroll", function() {
+
+    if(document.scrollingElement.scrollTop >= 50) {
+      logo.classList.add("logo-min");
+    } else {
+      logo.classList.remove("logo-min");
+    }
+
+  }, false)
+
+})();
