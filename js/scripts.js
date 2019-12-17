058201b5c18ae9d5869388a231868015c11e3001
Tomasz <tomekwebdev@gmail.com>
2019-12-17 23:16:47 +0100

    ready to upload to internet

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,4 @@
-$(document).ready(function(){
-
-  $('#carousel-example').on('slide.bs.carousel', function (e) {
-      /*
-          CC 2.0 License Iatek LLC 2018 - Attribution required
-      */
-      var $e = $(e.relatedTarget);
-      var idx = $e.index();
-      var itemsPerSlide = 5;
-      var totalItems = $('.carousel-item').length;
-
-      if (idx >= totalItems-(itemsPerSlide-1)) {
-          var it = itemsPerSlide - (totalItems - idx);
-          for (var i=0; i<it; i++) {
-              // append slides to end
-              if (e.direction=="left") {
-                  $('.carousel-item').eq(i).appendTo('.carousel-inner');
-              }
-              else {
-                  $('.carousel-item').eq(0).appendTo('.carousel-inner');
-              }
-          }
-      }
-  });
-
-});
+// Top nav scroll animation
 
 var logo = document.querySelector("#con-logo");
 
@@ -37,6 +12,6 @@ var logo = document.querySelector("#con-logo");
       logo.classList.remove("logo-min");
     }
 
-  }, false)
+  }, false);
 
 })();
